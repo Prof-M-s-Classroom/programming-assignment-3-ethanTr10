3398dcfefa66f310202652145150ece7a8483333
Ethan <etruong7035@sdsu.edu>
2025-05-06 22:32:09 -0700

    First bit of work!

--- heap.h
+++ heap.h
@@ -3,13 +3,48 @@
 
 class MinHeap {
 public:
-    MinHeap(int capacity);
-    ~MinHeap();
-    void insert(int vertex, int key);
-    int extractMin();
+    MinHeap(int capacity) {  //ethan's notes: constructor
+        this->capacity = capacity;
+        heapArray= new int[capacity]; //stores the first element of the array
+        keyArray= new int[capacity];
+        this->size = 0;
+    };
+
+    ~MinHeap(); //ethan's notes: destructor
+
+    void insert(int vertex, int key) {
+        // ethan's notes: must call minHeapify
+        heapArray[size] = vertex;
+        keyArray[size] = key;
+        size++;//[57, 23]
+        if (size > 1) { //if two or higher in the number of vertices
+            minHeapify(size);
+        }
+    };
+
+    int extractMin() {
+        return keyArray; //double check, should be returning the lowest key val?
+    }
+
     void decreaseKey(int vertex, int newKey);
-    bool isInMinHeap(int vertex);
-    bool isEmpty();
+
+    bool isInMinHeap(int vertex) {
+        for (int i=0; i< capacity; i++) {
+            if (heapArray[i] == vertex ) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    bool isEmpty() {
+        for (int i=0; i< capacity; i++) {
+            if (heapArray[i] != 0) {
+                return false;
+            }
+        }
+        return true;
+    };
 
 private:
     int* heapArray;        // Heap of vertex indices
@@ -18,7 +53,9 @@ private:
     int capacity;
     int size;
 
-    void minHeapify(int idx);
+    void minHeapify(int idx) {
+
+    };
 };
 
 #endif
