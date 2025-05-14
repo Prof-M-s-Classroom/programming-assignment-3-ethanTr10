1992962c4222bb39ce5235fcc5119f0fdcf54282
Ethan <etruong7035@sdsu.edu>
2025-05-13 20:12:58 -0700

    Still in progress

--- graph.h
+++ graph.h
@@ -2,47 +2,128 @@
 #define GRAPH_H
 using namespace std;
 
+#include "heap.h"
 class Graph {
 public:
-    Graph(int vertices) {
+    Graph(int vertices) {   //constructor for Graph involving the creating of 2D array for adjacency matrix and minHeap
         this->numVertices = vertices;
         adjMatrix= new int*[numVertices];
         for (int r = 0; r < numVertices; r++) {
-            adjMatrix[r] = new int[numVertices]; //this new int[numVertices] is the columns
+            adjMatrix[r] = new int[numVertices];    //this new int[numVertices] is the columns
         }
 
-        // for (int r = 0; r < numVertices; r++) {
-        //     for (int c = 0; c < numVertices; c++) {
-        //         adjMatrix[r][c] = {999}; //maybe include this so that it is not confusing with a weight of 0
-        //     }
-        // }
+        for (int r = 0; r < numVertices; r++) {
+            for (int c = 0; c < numVertices; c++) {
+                adjMatrix[r][c] = infinity;    //setting all the values at first to be the constant infinity
+            }
+        }
+        minHeap= new MinHeap(vertices);
+        totalCost=0;
     };
-    //~Graph(); FIX THIS
-    void addEdge(int u, int v, int weight) {
+    ~Graph() {};    //figure this out at some point
+    void addEdge(int u, int v, int weight) {    //set the weight at each slot r,c and c,r because it is undirected
         //u is the row
         //v is the col
         //weight is the value in there
         adjMatrix[u][v] = {weight};
         adjMatrix[v][u] = {weight};
     };
-    void primMST() {
-        cout<<"Adjacency Matrix"<<endl;
+    void primMST() { //prints out the adjacency matrix based on the given input
+        cout<<"Adjacency Matrix Based on Initial Input" <<endl;
         for (int c = 0; c < numVertices; c++) {
-            cout << "     " << c << "   ";
+            cout << "     " << c << "   ";  //prints out all column values
         }
         cout << endl;
-        for (int r = 0; r < numVertices; r++) {
-            cout << r << "    ";
-            for (int c = 0; c < numVertices; c++) {
-                cout << adjMatrix[r][c] << "        ";
+        for (int r = 0; r < numVertices; r++) {    //runs numVertices times, printing out the current row
+            cout << r << "  { ";
+            for (int c = 0; c < numVertices; c++) {    //responsible for printing value at adjMatrix[r][c]
+                if (adjMatrix[r][c] == infinity)  {
+                    cout<< "∞";
+                }
+                else {
+                    cout << adjMatrix[r][c];
+                }
+
+                if (c == numVertices - 1) {    //special case when reaching the last possible column for formatting
+                    cout << " }";
+                }
+                else {
+                    cout << "        ";
+                }
             }
             cout << endl;
         }
+
+        minHeap->setAllVisitedArray(infinity);
+        for (int r = 0; r < numVertices; r++) {
+            //used to say numVertices
+            for (int c = 0; c < numVertices; c++) { //used to say numVertices
+                tempVal=adjMatrix[r][c];
+                if (r ==0 && c==0) {
+                    minHeap->insert(infinity,infinity);
+                    minHeap->addToVisited(r);
+                }
+                else if (tempVal == infinity) {
+                    minHeap->insert(infinity,infinity);
+                }
+                else {
+                    minHeap->insert(c,tempVal);
+                }
+            }
+
+            for (int i=0; i< numVertices; i++) {
+                //Continuously runs until all options have been visited
+                int tempVal = minHeap->extractVisited(i);
+                if (minHeap->extractFromHeap(i) != tempVal ) {
+                    //minHeap->insertInPositionArray(r);
+                    tempMinVal= minHeap->extractMin();
+                    tempMinValIndex= minHeap->extractMinIndex(); //this gives the value with the lowest weight's index
+                    //then double check that the lowest weight associtaed vertex is not in the vistied array either
+                    int associatedVertexWithMinWeight= minHeap->extractFromHeap(tempMinValIndex);
+
+                    if (minHeap->isInPositionArray(associatedVertexWithMinWeight) == false) { //if index HAS NOT BEEN VISITED
+                        cout << r <<"--" << associatedVertexWithMinWeight << "(" << tempMinVal << ")" << endl;
+                        minHeap->addToVisited(associatedVertexWithMinWeight);
+                        totalCost= totalCost+ tempMinVal;
+                        break;
+                    }
+                    else { //if index HAS been VISITED
+                        minHeap->decreaseKey(associatedVertexWithMinWeight,0); //maybe need to do something so th
+                    }
+                }
+            }
+            //minHeap->clearAllArrays();
+        }
+
+        //minHeap->insert(v, weight);
+        // for (int r = 0; r < numVertices; r++) {
+        //     for (int c = 0; c < numVertices; c++) {
+        //         tempVal= adjMatrix[r][c];
+        //         if (minHeap->isInMinHeap(r)==true && tempVal!=infinity) { //it is 0 atm
+        //             minHeap->insert(c, tempVal);
+        //         }
+        //         else {
+        //             minHeap->incrementCounter();
+        //         }
+        //     }
+        //     tempMinVal= minHeap->extractMin();
+        //     minHeap->decreaseKey(1,2); //use this to get rid of the
+        //
+        //     //after extracting the min which would be 0 at this point, you then gotta call decrease key which works togethwer with minheapify
+        // }
+
+        cout<< "\nTotal Cost: " << minHeap->getTotalCost() << endl;
     };  // Must print MST edges and total weight
 
 private:
     int** adjMatrix;
     int numVertices;
+    int tempVal;
+    const int infinity = 999;
+    int tempMinVal;
+    int tempMinValIndex;
+    MinHeap *minHeap;
+    int totalCost;
 };
 
 #endif
--- heap.h
+++ heap.h
@@ -7,26 +7,105 @@ public:
         this->capacity = capacity;
         heapArray= new int[capacity]; //stores the first element of the array
         keyArray= new int[capacity];
+        visited= new int[capacity];
+        unvisited= new int[capacity];
+        position= new int[capacity];
+        this->totalCost=0;
+        this->indexOfVisited=0;
         this->size = 0;
+        this->indexOfMinHeap=0;
     };
 
     ~MinHeap(); //ethan's notes: destructor
 
     void insert(int vertex, int key) {
         // ethan's notes: must call minHeapify
-        heapArray[size] = vertex;
-        keyArray[size] = key;
-        size++;//[57, 23]
-        if (size > 1) { //if two or higher in the number of vertices
+        if (key==0) { //goes with the assumption that none of the weights will be 0 between vertices
+            keyArray[size]=key; //keys should be the weight
+            heapArray[size]=vertex; //
+            //position[size]=vertex;
+            visited[sizeForVisitedArray]=vertex;
+            sizeForVisitedArray++;
+            size++;
+        }
+        else {
+            //position[size]=vertex;
+            heapArray[size] = vertex;
+            keyArray[size] = key;
+            size++;//[57, 23]
+        }
+        if (size%4==0) { //if two or higher in the number of vertices
             minHeapify(size);
         }
+
+
     };
 
-    int extractMin() {
-        return keyArray; //double check, should be returning the lowest key val?
+    void insertInMinHeap(int vertex) {
+        heapArray[indexOfMinHeap] = vertex;
     }
 
-    void decreaseKey(int vertex, int newKey);
+    void insertInPositionArray(int vertex) {
+        position[indexOfMinHeap] = vertex;
+    }
+
+    int extractMin() { //returns the index with the lowest weight
+
+        int tempMin=keyArray[0];
+        for (int i=0; i < size; i++) {
+            if (keyArray[i] < tempMin) {
+                tempMin = keyArray[i];
+            }
+        }
+        return tempMin;
+    }
+
+    int extractMinIndex() { //returns the index with the lowest weight
+        int minIndex=0;
+        int tempMin=keyArray[0];
+        for (int i=0; i < size; i++) {
+            if (keyArray[i] < tempMin) {
+                tempMin = keyArray[i];
+                minIndex = i;
+            }
+        }
+        return minIndex;
+    }
+
+    int extractFromHeap(int index) {
+        return heapArray[index];
+    }
+
+    int extractKey(int index) {
+        return keyArray[index];
+    }
+
+    int extractVisited(int index) {
+        return visited[index];
+    }
+
+    void decreaseKey(int vertex, int newKey) {
+        keyArray[vertex] = newKey;
+    };
+
+    void setAllVisitedArray(int value) {
+        for (int i=0; i < capacity; i++) {
+            visited[i] = value;
+        }
+    }
+
+    void clearAllArrays() {
+        for (int i=0; i < capacity; i++) {
+            keyArray[i] = 0;
+            heapArray[i] = 0;
+            position[i] = 999;
+        }
+    }
+
+    void addToVisited(int value) {
+        visited[indexOfVisited] = value;
+        indexOfVisited++;
+    }
 
     bool isInMinHeap(int vertex) {
         for (int i=0; i< capacity; i++) {
@@ -37,6 +116,16 @@ public:
         return false;
     }
 
+    bool isInPositionArray(int vertex) {
+        for (int i=0; i< capacity; i++) {
+            if (position[i] == vertex ) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
     bool isEmpty() {
         for (int i=0; i< capacity; i++) {
             if (heapArray[i] != 0) {
@@ -45,14 +134,56 @@ public:
         }
         return true;
     };
+    int getTotalCost() {
+        cout<< "keyArray contains ";
+        for (int i=0; i< capacity; i++) {
+            cout << keyArray[i] << " ";
+        }
+
+        cout << "\n" << endl;
+
+        cout<< "heapArray contains ";
+        for (int i=0; i< capacity; i++) {
+            cout << heapArray[i] << " ";
+        }
+        cout << "\n" << endl;
+
+        cout<< "positionArray contains ";
+        for (int i=0; i< capacity; i++) {
+            cout << position[i] << " ";
+        }
+
+        cout << "\n" << endl;
+        cout<< "visited contains ";
+        for (int i=0; i< capacity; i++) {
+            cout << visited[i] << " ";
+        }
+        cout << "\n" << endl;
+
+        return totalCost;
+    }
+
+    void incrementCounter() {
+        counter++;
+    }
 
 private:
     int* heapArray;        // Heap of vertex indices
     int* keyArray;         // Corresponding key values
     int* position;         // Maps vertex to its position in heap
+
+    int* visited;
+    int* unvisited;
+
+
+
+    int indexOfMinHeap;
     int capacity;
     int size;
-
+    int sizeForVisitedArray;
+    int counter;
+    int indexOfVisited;
+    int totalCost; //DELETE ALL OF TOTAL COST FROM HEAP.H....ITS LITERALLY ALL JUST FOR TESTING
     void minHeapify(int idx) {
 
     };
